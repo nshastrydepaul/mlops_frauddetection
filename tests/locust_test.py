ec516360c0c02c34ceb6f65b580fdea6c27e2b79
trainee-io <musa17ddiq@gmail.com>
2026-06-06 23:41:46 -0500

    feat(phase3): add monitoring setup, load test results, and GCP evidence

---
+++ tests/locust_test.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, task, between
+
+class FraudAPIUser(HttpUser):
+    host = "https://fraud-detection-predict-lj4hg275tq-uc.a.run.app"
+    wait_time = between(1, 2)
+
+    @task(3)
+    def health_check(self):
+        self.client.get("/health")
+
+    @task(2)
+    def list_models(self):
+        self.client.get("/models")
+
+    @task(5)
+    def predict_simple(self):
+        self.client.post(
+            "/predict/simple?amt=5000&merchant_risk_30_day=28&trans_time_is_night=1&avg_amt_per_customer=50"
+        )
